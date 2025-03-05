d85d721f95dca0749cebbae054681f5aac9c05f1
Frikk Brændsrød <fribra05@yahoo.com>
2025-03-05 11:39:49 +0100

    Added getDeck method aswell as mixed constructor to create full deck of cards

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -2,15 +2,50 @@ package no.ntnu.idatx2003.oblig4.cardgame;
 
 import javax.smartcardio.Card;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class DeckOfCards {
-  private ArrayList<Integer> deck;
+  private ArrayList<String> deck;
 
 
-  private DeckOfCards() {
-    for(int i = 1; i <= 13; i++) {
-      deck = new ArrayList<>();
-      deck.add(i+);
+  DeckOfCards() {
+    int suitCounter = 0;
+    deck = new ArrayList<>();
+    for(int i = 1; i <= 14; i++) {
+      if(i == 14){
+        suitCounter++;
+        if(suitCounter == 4){
+          break;
+        }
+        i = 1;
+
+      }
+
+      switch(suitCounter) {
+        case 0:
+          deck.add(i + "S");
+          break;
+
+        case 1:
+          deck.add(i + "H");
+          break;
+
+        case 2:
+          deck.add(i + "D");
+          break;
+
+        case 3:
+          deck.add(i + "C");
+          break;
+      }
     }
+  }
+
+  public void shuffle(ArrayList<String> deck) {
+    Collections.shuffle(deck);
+  }
+
+  public ArrayList<String> getDeck() {
+    return deck;
   }
 }
