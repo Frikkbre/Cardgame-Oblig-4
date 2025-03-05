2d6cb835e4eaf1c20ba0f85b3a0644edfc5c52b9
Frikk Brændsrød <fribra05@yahoo.com>
2025-03-05 11:18:26 +0100

    Added class to hold list of all cards as DeckOfCards

---
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -0,0 +1,16 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+
+public class DeckOfCards {
+  private ArrayList<Integer> deck;
+
+
+  private DeckOfCards() {
+    for(int i = 1; i <= 13; i++) {
+      deck = new ArrayList<>();
+      deck.add(i+);
+    }
+  }
+}
