d4d6b470615763208115edd9c20fd6e59389ca8c
Frikk Brændsrød <fribra05@yahoo.com>
2025-03-05 12:02:37 +0100

    Added method dealHand

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 
 public class DeckOfCards {
   private ArrayList<String> deck;
+  private ArrayList<String> hand;
 
 
   DeckOfCards() {
@@ -44,6 +45,28 @@ public class DeckOfCards {
   public void shuffle(ArrayList<String> deck) {
     Collections.shuffle(deck);
   }
+
+  /**
+   * Deals n cards from the remaining cards in the deck
+   * checks if deck has more than 1 and under 52 cards and throws exception if not
+   * @return
+   */
+  public void dealHand(int n) {
+    hand = new ArrayList<>();
+    if(deck.size() < 1 || deck.size() > 52) {
+      throw new IllegalArgumentException("Deck must have more than 1 and less than 52 cards");
+    } else if (n < 1 || n > deck.size()) {
+      throw new IllegalArgumentException("you must deal a hand between 1 and 52 cards");
+    } else if (n > deck.size()) {
+      throw new IllegalArgumentException("Not enough cards in deck to deal a hand of " + n + " cards");
+    }
+    for(int i = 0; i < n; i++) {
+      hand.add(deck.get(i));
+      deck.remove(i);
+    }
+  }
+
+
 
   public ArrayList<String> getDeck() {
     return deck;
