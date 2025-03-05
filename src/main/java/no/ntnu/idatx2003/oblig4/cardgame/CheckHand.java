0e6c7dcf69b386509db1191ef5d8093a99195273
Frikk Brændsrød <fribra05@yahoo.com>
2025-03-05 12:56:34 +0100

    added doc and methods for all required checks

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/CheckHand.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/CheckHand.java
@@ -1,4 +1,60 @@
 package no.ntnu.idatx2003.oblig4.cardgame;
 
+/**
+ * This class is used to check the hand of a player
+ * Checks hand for:
+ * - 5 card flush (all cards have the same suit) using stream
+ * - queen of spades
+ * - sum of cards (ace = 1)
+ * - display only the cards with suit heart
+ */
 public class CheckHand {
+
+  /**
+   * Checks if the hand is a flush
+   * @param hand the hand to check
+   * @return true if the hand is a flush, false otherwise
+   */
+  public static boolean isFlush(PlayingCard[] hand) {
+    return hand.length == 5 && hand[0].getSuit() == hand[1].getSuit() && hand[1].getSuit() == hand[2].getSuit() && hand[2].getSuit() == hand[3].getSuit() && hand[3].getSuit() == hand[4].getSuit();
+  }
+
+  /**
+   * Checks if the hand contains the queen of spades
+   * @param hand the hand to check
+   * @return true if the hand contains the queen of spades, false otherwise
+   */
+  public static boolean hasQueenOfSpades(PlayingCard[] hand) {
+    for (PlayingCard card : hand) {
+      if (card.getFace() == 12 && card.getSuit() == 'S') {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Calculates the sum of the cards in the hand
+   * @param hand the hand to calculate the sum of
+   * @return the sum of the cards in the hand
+   */
+  public static int sumOfCards(PlayingCard[] hand) {
+    int sum = 0;
+    for (PlayingCard card : hand) {
+      sum += card.getFace();
+    }
+    return sum;
+  }
+
+  /**
+   * Displays only the cards with suit heart
+   * @param hand the hand to display
+   */
+  public static void displayHearts(PlayingCard[] hand) {
+    for (PlayingCard card : hand) {
+      if (card.getSuit() == 'H') {
+        System.out.println(card.getAsString());
+      }
+    }
+  }
 }
