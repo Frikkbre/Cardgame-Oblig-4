15ba59d7b82beddda736bf33c2b75d47f3b0922b
Frikk <fribra05@yahoo.com>
2025-03-07 19:58:27 +0100

    Added controller class and exit method to exit program from top menu

---
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Controller.java
@@ -0,0 +1,12 @@
+package no.ntnu.idatx2003.oblig4.cardgame;
+
+public class Controller {
+
+
+    /**
+     * Used to exit the program from the top menu
+     */
+    public void exit() {
+        System.exit(0);
+    }
+}
