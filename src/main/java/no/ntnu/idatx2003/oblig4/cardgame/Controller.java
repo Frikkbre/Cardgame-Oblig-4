da917ccfc71d6bc02f85283e99b5a7fabfb2e02e
Frikk <fribra05@yahoo.com>
2025-03-07 21:16:44 +0100

    Implemented checkAll method as event when pressing check hand button

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Controller.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Controller.java
@@ -1,11 +1,37 @@
 package no.ntnu.idatx2003.oblig4.cardgame;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
+import javax.smartcardio.Card;
 
 public class Controller {
+
+    private CardGame cardGame;
+    private DeckOfCards deckOfCards;
+    private CheckHand checkHand;
+
+    public Controller(CardGame cardGame, DeckOfCards deckOfCards, CheckHand checkHand) {
+        this.cardGame = cardGame;
+        this.deckOfCards = deckOfCards;
+        this.checkHand = checkHand;
+    }
+
+    public void drawhand() {
+        deckOfCards.dealHand(5); //Hardcoded to draw 5 cards
+    }
+
+    /**
+     * Is triggered when the user presses the "Check hand" button
+     */
+    public void checkHand(){
+        checkHand.checkAll(deckOfCards);
+    }
+
 
     /**
      * Used to exit the program from the top menu
      */
-    public void exit() {
+    public void exit() { //TODO - Fix this method
         System.exit(0);
     }
 }
